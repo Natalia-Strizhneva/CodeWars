7dd1c3dc041786d84f7022004ff25231e9b57c42
Natalia-Strizhneva <homosum07@gmail.com>
2023-07-07 23:09:20 +0300

    '8'

---
+++ 8 kyu/You only need one.js
@@ -0,0 +1,5 @@
+//Включает ли массив а значение х?
+
+function check(a, x) {
+  return a.includes(x);
+}
--- 8 kyu/arr includes.js
+++
@@ -1,5 +0,0 @@
-function check(a, x) {
-  return a.length == 0 ? false : true;
-}
-
-check([66, 101], 66);
